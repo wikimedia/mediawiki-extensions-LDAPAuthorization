149b7c0591795c8c3fae0068f2e7a602b1944453
Thomas Daniels <daniels.thomas@pm.me>
2021-09-22 15:34:31 +0200

    Add requirement to check if user matches LDAP query
    This commit allows to add a 'query' config value to 'rules' which
    accepts an arbitrary LDAP query. When a user tries to log in, the
    extension checks if the user matches that query and grants access if it
    does.
    
    Bug: T280875
    Change-Id: I8031a5cd6d7c1dc5eb4c0b525b9e68aa7c8ae0bc

---
+++ src/Requirement/LdapQuery.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace MediaWiki\Extension\LDAPAuthorization\Requirement;
+
+use MediaWiki\Extension\LDAPAuthorization\IRequirement;
+
+use MWException;
+
+/**
+ * A requirement that checks if the user matches a given LDAP query.
+ */
+class LdapQuery implements IRequirement {
+
+	/**
+	 *
+	 * @var \MediaWiki\Extension\LDAPProvider\Client
+	 */
+	protected $ldapClient = null;
+
+	/**
+	 *
+	 * @var string
+	 */
+	protected $userdn = null;
+
+	/**
+	 *
+	 * @var string
+	 */
+	protected $query = null;
+
+	/**
+	 *
+	 * @param \MediaWiki\Extension\LDAPProvider\Client $ldapClient
+	 * @param string $userdn
+	 * @param string $query
+	 */
+	public function __construct( $ldapClient, $userdn, $query ) {
+		$this->ldapClient = $ldapClient;
+		$this->userdn = $userdn;
+		$this->query = $query;
+	}
+
+	/**
+	 *
+	 * @return bool
+	 */
+	public function isSatisfied() {
+		try {
+			$entries = $this->ldapClient->search(
+				"(" . $this->query . ")",
+				$this->userdn,
+				[ "dn" ]
+			);
+		} catch ( MWException $e ) {
+			# For example a malformed query in the configuration.
+			wfDebugLog(
+				"LDAPAuthorization", "Could not check user against LDAP query: " . $e->getMessage()
+			);
+			return false;
+		}
+
+		return $entries["count"] > 0;
+	}
+}
